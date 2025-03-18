a7844d784827116031d0070c57384fcf34ee40f9
marvin nyarenchi <marvin.nyarenchi@github.com>
2025-03-18 13:09:24 +0300

    setup respond to ping handle connection

--- sider/src/main.rs
+++ sider/src/main.rs
@@ -1,16 +1,28 @@
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("Accepted new connection");
+            Ok(mut stream) => {
+                handle_connection(&mut stream);
             }
             Err(e) => {
                 println!("error:{}", e);
             }
         }
     }
+}
+
+fn handle_connection(stream: &mut TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    println!("Recieved: {:?}", buffer);
+
+    let response = "+PONG\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
